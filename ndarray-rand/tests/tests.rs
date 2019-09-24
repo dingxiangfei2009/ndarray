e284490fc78ccc08b3a401e9e48fdc44f56084a5
bluss <bluss@users.noreply.github.com>
2019-09-24 19:26:17 +0200

    FEAT: Use from_shape_fn_memory_order in ndarray-rand
    This simplifies random/random_using methods by skipping the index
    iteration in the constructor, which is unused anyway and only used for
    figuring out how many elments to make.

--- ndarray-rand/tests/tests.rs
+++ ndarray-rand/tests/tests.rs
@@ -1,4 +1,5 @@
 use ndarray::Array;
+use ndarray::ShapeBuilder;
 use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
 
@@ -11,6 +12,21 @@ fn test_dim() {
             assert_eq!(a.shape(), &[m, n]);
             assert!(a.iter().all(|x| *x < 2.));
             assert!(a.iter().all(|x| *x >= 0.));
+            assert!(a.is_standard_layout());
+        }
+    }
+}
+
+#[test]
+fn test_dim_f() {
+    let (mm, nn) = (5, 5);
+    for m in 0..mm {
+        for n in 0..nn {
+            let a = Array::random((m, n).f(), Uniform::new(0., 2.));
+            assert_eq!(a.shape(), &[m, n]);
+            assert!(a.iter().all(|x| *x < 2.));
+            assert!(a.iter().all(|x| *x >= 0.));
+            assert!(a.t().is_standard_layout());
         }
     }
 }
